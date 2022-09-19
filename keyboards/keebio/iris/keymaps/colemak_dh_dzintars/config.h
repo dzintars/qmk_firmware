cf391ecc196b16fe8f5a3a577b893984d7df2589
Dzintars Klavins <dzintars.klavins@gmail.com>
2022-09-19 13:57:13 +0300

    feat(keebio/iris): init personal colemak layout

---
+++ keyboards/keebio/iris/keymaps/colemak_dh_dzintars/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xCB10
+#define PRODUCT_ID      0x5256
+#define DEVICE_VER      0x0500
+#define MANUFACTURER    Keebio
+#define PRODUCT         Iris Rev. 5
+#define DESCRIPTION     Split 50 percent ergonomic keyboard
+
+// #define DYNAMIC_KEYMAP_LAYER_COUNT 5
