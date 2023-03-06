6df6cbaca639116ff48895d39d4ac58e6bba4e04
Dzintars Klavins <dzintars.klavins@gmail.com>
2023-03-06 17:54:30 +0200

    chore(colemak_dh_dzintars): remove definitions
    There is an error `error: "MANUFACTURER" redefined.... ` when compiling.

--- keyboards/keebio/iris/keymaps/colemak_dh_dzintars/config.h
+++ keyboards/keebio/iris/keymaps/colemak_dh_dzintars/config.h
@@ -4,8 +4,6 @@
 #define VENDOR_ID       0xCB10
 #define PRODUCT_ID      0x5256
 #define DEVICE_VER      0x0500
-#define MANUFACTURER    Keebio
-#define PRODUCT         Iris Rev. 5
 #define DESCRIPTION     Split 50 percent ergonomic keyboard
 
 // #define DYNAMIC_KEYMAP_LAYER_COUNT 5
