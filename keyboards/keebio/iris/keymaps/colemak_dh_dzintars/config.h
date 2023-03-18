3a642a787da449cf53e3f28e04bde447054d24ac
Dzintars Klavins <dzintars.klavins@gmail.com>
2023-03-18 21:41:52 +0200

    chore(colemak_dh_dzintars): replace tapping_force_hold
    https://docs.qmk.fm/#/ChangeLog/20230226?id=quick-tap-term

--- keyboards/keebio/iris/keymaps/colemak_dh_dzintars/config.h
+++ keyboards/keebio/iris/keymaps/colemak_dh_dzintars/config.h
@@ -8,11 +8,9 @@
 
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 200
-
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
-
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
 
 // #define DYNAMIC_KEYMAP_LAYER_COUNT 5
